4351c181f4cd0d6bc65407697c601f1755d29e06
Maliha Hossain <malu.hoss@gmail.com>
2019-04-10 20:12:09 +0000

    displayed username in Chatbar instead of Type your name

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,22 @@ import ChatBar from './ChatBar.jsx';
 
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+       messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }]
+      };
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -12,7 +28,7 @@ export default class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList/>
-        <ChatBar/>
+        <ChatBar user={this.state.currentUser}/>
       </React.Fragment>
     );
   }
