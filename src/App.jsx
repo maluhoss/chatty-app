69c7d4d18f8c28d3de90e74b2e811322b8ae6895
Maliha Hossain <malu.hoss@gmail.com>
2019-04-10 18:17:52 +0000

    created new jsx files to create new react components for each section of webpage, added webpack script to html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
-class App extends Component {
+
+export default class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <React.Fragment>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList/>
+        <ChatBar/>
+      </React.Fragment>
     );
   }
 }
-export default App;
---
+++ src/Message.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Message extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">I won/'t be impressed with technology until I can download food.</span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </React.Fragment>
+    );
+  }
+}
