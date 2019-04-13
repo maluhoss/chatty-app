cce71448a73d9d6a8d2625f72c340c6456b30713
Maliha Hossain <malu.hoss@gmail.com>
2019-04-13 08:08:05 +0000

    modified display of onthe word user to show users if more than 1 user online

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 
 export default class NavBar extends Component {
+  displayUserWord = () => {
+    let users = '';
+    if (this.props.onlineUsers === 1) {
+      users = 'User';
+    } else {
+      users = 'Users';
+    }
+
+    return users;
+  }
+
   render() {
     return (
       <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
-          <span className="navbar-users">{this.props.onlineUsers} Users Online</span>
+          <span className="navbar-users">{this.props.onlineUsers} {this.displayUserWord()} Online</span>
         </nav>
     );
   }
